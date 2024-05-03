a8ce1ddd1219e631746b4b27d6888f4084be7126
isha7805 <156116699+isha7805@users.noreply.github.com>
2024-05-03 13:30:36 +0530

    correction in  index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-import inquirer from 'inquirer';
-import qr from 'qr-image';
-import fs from 'fs'.;
 
-console.log(`\n+-----------------------------------------------------+`);
-console.log()
-inquirer
-  .prompt([
-    {
-        "message" : "Type in your URL : ",
-        "name" : "URL"
-    }
-  ])
-  .then((answers) => {
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream('qr_img.png'));
